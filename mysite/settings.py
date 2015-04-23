6747181ddb04e239cc7f3a628abda3f5d161a4b3
Nithya <nithya.murali23@gmail.com>
2015-04-23 14:41:49 -0400

    more revisions to settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -14,7 +14,13 @@ import os
 import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_URL = '/static/'
+STATIC_ROOT = 'staticfiles'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 
 # Quick-start development settings - unsuitable for production
@@ -110,11 +116,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/dev/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = '/static/'
 
 #please change the resolute path here
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static"),'/var/www/static/','/C:/Users/Nithya/Documents/mysite/static/']
+#STATICFILES_DIRS = [os.path.join(BASE_DIR, "static"),'/var/www/static/','/C:/Users/Nithya/Documents/mysite/static/']
 
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
