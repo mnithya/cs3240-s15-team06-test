3ba0dbcb70f8d4dba14b6da40ee5c539701ae360
Nithya <nithya.murali23@gmail.com>
2015-04-24 14:50:25 -0400

    change timezone

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ STATICFILES_DIRS = (
 )
 
 
-# Quick-start development settings - unsuitable for production
+# Quick-starlt development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/dev/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
@@ -104,7 +104,7 @@ DATABASES['default']['ENGINE'] = 'django_postgrespool'
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'EST'
 
 USE_I18N = True
 
